9157fc09a0a4a4ecdbcb11d5d90c99e70dfeca02
Vincent C <vincent.callede@outlook.fr>
2020-12-12 12:01:20 +0100

    Routes and composers first modification

---
+++ src/Controller/FilmController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class FilmController extends AbstractController{
+
+    public function menu(){
+        return $this->render('menu.html.twig');
+    }
+}
