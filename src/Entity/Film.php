84f4bc27766c8b177101cb16e3df2f0df0823530
Vincent C <vincent.callede@outlook.fr>
2020-12-12 12:39:49 +0100

    Wrong changes

---
+++ src/Entity/Film.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+class Film
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="smallint", unique=true)
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateSortie;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=true)
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $ageMinimal;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Acteur::class, mappedBy="film")
+     */
+    private $acteurs;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Genre::class, inversedBy="films")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $genre;
+
+    public function __construct()
+    {
+        $this->acteurs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDateSortie(): ?\DateTimeInterface
+    {
+        return $this->dateSortie;
+    }
+
+    public function setDateSortie(\DateTimeInterface $dateSortie): self
+    {
+        $this->dateSortie = $dateSortie;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getAgeMinimal(): ?int
+    {
+        return $this->ageMinimal;
+    }
+
+    public function setAgeMinimal(int $ageMinimal): self
+    {
+        $this->ageMinimal = $ageMinimal;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Acteur[]
+     */
+    public function getActeurs(): Collection
+    {
+        return $this->acteurs;
+    }
+
+    public function addActeur(Acteur $acteur): self
+    {
+        if (!$this->acteurs->contains($acteur)) {
+            $this->acteurs[] = $acteur;
+            $acteur->addFilm($this);
+        }
+
+        return $this;
+    }
+
+    public function removeActeur(Acteur $acteur): self
+    {
+        if ($this->acteurs->removeElement($acteur)) {
+            $acteur->removeFilm($this);
+        }
+
+        return $this;
+    }
+
+    public function getGenre(): ?Genre
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(?Genre $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+}
